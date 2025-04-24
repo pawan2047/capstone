be3ede9da38b55c32b8e57f659de7080d6c50d34
SuyoshaA <121407982+SuyoshaA@users.noreply.github.com>
2025-04-24 01:06:16 -0500

    Create api_keys.php

---
+++ api_keys.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'firebase_api_key' => 'TFZNe5onqZ97HNmY3T6OwVzvypbZ99daMiRx5Pax',
+    'openai_api_key' => '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',
+];
+?>
